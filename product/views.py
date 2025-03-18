29225bebf02706f27fa2b01231bbf2fb7e8c3b35
lllDavid <imperfectrising5527@gmail.com>
2025-03-18 16:33:16 +0100

    add product views

--- product/views.py
+++ product/views.py
@@ -1,3 +1,19 @@
+from django.views.generic import ListView, DetailView
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Product
+
+def home(request):
+    return render(request, 'home.html')
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'product-list.html'
+    context_object_name = 'products'
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'product-detail.html'
+    context_object_name = 'product'
+
+
