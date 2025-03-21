bc6cc65bc510bebc18e59ccfcdcd440aa80ba291
lllDavid <imperfectrising5527@gmail.com>
2025-03-21 16:21:59 +0100

    add dynamic product quantity dropdown

--- product/views.py
+++ product/views.py
@@ -12,5 +12,13 @@ class ProductDetailView(DetailView):
     model = Product
     template_name = 'product-detail.html'
     context_object_name = 'product'
+    # Get the stock range for dynamic quantity option in dropdown
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        product = self.object
+        context['stock_range'] = range(1, product.stock + 1)
+        return context
+
+
 
 
