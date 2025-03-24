e35f3292e7c0de6321e9be2356229d0573254308
lllDavid <imperfectrising5527@gmail.com>
2025-03-24 16:12:28 +0100

    add redirect after registration

--- user/views.py
+++ user/views.py
@@ -1,4 +1,7 @@
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
+from django.contrib import messages
+
 from .models import StoreUser  
 
 def create_user(request):
@@ -6,11 +9,23 @@ def create_user(request):
         email = request.POST.get('email')
         username = request.POST.get('username')
         password = request.POST.get('password')
-        user_manager = StoreUser.objects  
-        user = user_manager.create_user(email, username, password)
-        return redirect('home')
-    
+
+        try:
+            user = StoreUser.objects.create_user(email=email, username=username, password=password)
+
+            user = authenticate(request, email=email, password=password)
+
+            if user is not None:
+                login(request, user)  
+                return redirect('home') 
+            else:
+                messages.error(request, "Authentication failed. Please try again.")
+        except Exception as e:
+            messages.error(request, f"Error: {str(e)}")
+
     return render(request, 'registration/register.html')
+
+
 
 '''
 def create_superuser(request):
